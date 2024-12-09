ac3ea7662265d6bc4e79ef08165e6454fd7a918b
tamara-kaye <tamarakaye9@gmail.com>
2024-12-09 10:48:20 +0200

    code with additional comment

--- module.tf
+++ module.tf
@@ -14,6 +14,8 @@ provider "aws" {
 }
 
 # Data: Get latest Ubuntu AMI
+#Ubuntu = A popular open source operating system based on Linux
+#AMI = A template provided by AWS that contains the information needed to run an instance in AWS
 data "aws_ami" "aws_ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical's AWS account ID for Ubuntu AMIs
@@ -68,6 +70,32 @@ resource "aws_subnet" "public_subnet_2" {
   }
 }
 
+#better way- do it with for_each: 
+# resource "aws_subnet" "public_subnet" {
+#   for_each = {
+#     public_subnet_a = {
+#       cidr_block        = "10.0.1.0/24"
+#       availability_zone = "eu-central-1a"
+#       name              = "public-subnet-a"
+#     }
+#     public_subnet_b = {
+#       cidr_block        = "10.0.3.0/24"
+#       availability_zone = "eu-central-1b"
+#       name              = "public-subnet-b"
+#     }
+#   }
+
+#   vpc_id                  = aws_vpc.default.id
+#   cidr_block              = each.value.cidr_block
+#   map_public_ip_on_launch = true
+#   availability_zone       = each.value.availability_zone
+
+#   tags = {
+#     Name = each.value.name
+#   }
+# }
+
+
 # Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.default.id
@@ -102,6 +130,8 @@ resource "aws_route_table" "public_route_table" {
     Name = "public-route-table"
   }
 }
+
+### Public subnet -> Route table -> Internet gateway
 
 # Associate Public Subnet with Public Route Table
 resource "aws_route_table_association" "public_subnet_association" {
@@ -138,17 +168,19 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
+### Private subnet -> route table -> NAT gateway -> Internet
+
 # Associate Private Subnet with Private Route Table
 resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
-# Security Group for Load Balancer
+# Security Group for Load Balancer   #Who can access and on which ports
 resource "aws_security_group" "lb_sg" {
   vpc_id = aws_vpc.default.id
 
-  ingress {
+  ingress { #Allows incoming HTTP traffic on PORT 80 from anywhere on the Internet
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -162,7 +194,7 @@ resource "aws_security_group" "lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+  egress { #Allows all traffic leaving the LB to any destination
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -178,7 +210,7 @@ resource "aws_security_group" "lb_sg" {
 resource "aws_lb" "app_lb" {
   name               = "app-lb"
   internal           = false
-  load_balancer_type = "application"
+  load_balancer_type = "application" #Will handle HTTP\HTTPS traffic
   security_groups    = [aws_security_group.lb_sg.id]
   subnets = [
     aws_subnet.public_subnet.id,
@@ -201,25 +233,27 @@ resource "aws_lb_target_group" "app_tg" {
 
   health_check {
     path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
+    interval            = 30    #Time in seconds between requests
+    timeout             = 5     #The time in seconds that the ALB will wait for a response
+    healthy_threshold   = 2     #The number of successful health checks required for the target to be considered healthy
+    unhealthy_threshold = 2     #The number of failed checks for the target to sit as unhealthy
+    matcher             = "200" #http status code that the ALB expects in a health check response
   }
 }
 
-# Listener for Load Balancer
+# Listener for Load Balancer  #A set of rules that determine how incoming traffic to the ALB should be handled
 resource "aws_lb_listener" "app_listener" {
   load_balancer_arn = aws_lb.app_lb.arn
   port              = 80
-  protocol          = "HTTP"
+  protocol          = "HTTP" #listens for http requests
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+
+### ALB -> listener -> Target group -> EC2 instance
 
 # Security Group for Private EC2 Instance
 resource "aws_security_group" "private_instance_sg" {
@@ -228,15 +262,15 @@ resource "aws_security_group" "private_instance_sg" {
   ingress {
     from_port       = 80
     to_port         = 80
-    protocol        = "tcp"
+    protocol        = "tcp" #for http
     security_groups = [aws_security_group.lb_sg.id]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"          #all protocols
+    cidr_blocks = ["0.0.0.0/0"] #can send traffic to any IP
   }
 
   tags = {
@@ -245,11 +279,11 @@ resource "aws_security_group" "private_instance_sg" {
 }
 
 # EC2 Instance in Private Subnet
-resource "aws_instance" "aws_ubuntu" {
+resource "aws_instance" "aws_ubuntu" { #ubuntu that runs EC2 instance
   instance_type = "t2.micro"
   ami           = data.aws_ami.aws_ubuntu.id
   #key_name                    = var.key_name
-  user_data                   = file("userdata.tpl")
+  user_data                   = file("userdata.tpl") #nginx file
   security_groups             = [aws_security_group.private_instance_sg.id]
   associate_public_ip_address = false
   subnet_id                   = aws_subnet.private_subnet.id
