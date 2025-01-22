f79a12e0dccc1c3d66e7e415b86320429f2fc742
tamara-kaye <tamarakaye9@gmail.com>
2025-01-22 16:45:15 +0200

    adding pipeline

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -1,4 +1,5 @@
 # This file is maintained automatically by "terraform init".
+# Downloads the required providers and locks their versions.
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/aws" {
--- terraform/module.tf
+++ terraform/module.tf
@@ -181,14 +181,14 @@ resource "aws_security_group" "lb_sg" {
   vpc_id = aws_vpc.default.id
 
   ingress { #Allows incoming HTTP traffic on PORT 80 from anywhere on the Internet
-    from_port   = 80
+    from_port   = 80 
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 443
+    from_port   = 443 #HTTPS
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
